6dca6f46ea2c30deeedb217db51a0fe7e8fe5d89
dafuq360 <theoplaysmc@gmail.com>
2017-02-15 07:53:59 -0800

    t

--- src/main/java/me/creepinson/item/Syringe.java
+++ src/main/java/me/creepinson/item/Syringe.java
@@ -3,14 +3,19 @@ package me.creepinson.item;
 import me.creepinson.handlers.ItemHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteract;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class Syringe extends ModItems{
 	private Minecraft mc = Minecraft.getMinecraft();
@@ -18,14 +23,30 @@ public class Syringe extends ModItems{
 		super(name, tab);
 		
 	}
+			@Override
 	public ActionResult<ItemStack> onItemRightClick(ItemStack item, World world, EntityPlayer user, EnumHand hand)
 
 	{
+			    RayTraceResult raytrace = user.rayTrace(10, 30);
+			    
+			    if(raytrace.entityHit instanceof EntityZombie){
+			    	
+			    	user.inventory.deleteStack(item);
+					user.inventory.addItemStackToInventory(new ItemStack(ItemHandler.Syringe_Full_Zombie, 1));
+			 
+					
+					new ActionResult<ItemStack>(EnumActionResult.SUCCESS, item);
+			    }else{
+			    	user.inventory.deleteStack(item);
+					user.inventory.addItemStackToInventory(new ItemStack(ItemHandler.Syringe_Full_Player, 1));
+			    
+					return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, item);
+			    }
+			    return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, item);
+			}
 		
-	user.inventory.deleteStack(item);
-		user.inventory.addItemStackToInventory(new ItemStack(ItemHandler.Syringe_Full_Player, 1));
-	return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, item);
-	}
-}
+		
+		}
+	
 
 
--- src/main/java/me/creepinson/item/Syringe_Full_Zombie.java
+++ src/main/java/me/creepinson/item/Syringe_Full_Zombie.java
@@ -1,14 +1,46 @@
 package me.creepinson.item;
 
+import java.util.List;
+
+import me.creepinson.handlers.ItemHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class Syringe_Full_Zombie extends ModItems{
 
 	private Minecraft mc = Minecraft.getMinecraft();
 	public Syringe_Full_Zombie(String name, CreativeTabs tab) {
 		super(name, tab);
-		
+		   setHasSubtypes(true); // This allows the item to be marked as a metadata item.
+	        setMaxDamage(0);
 	}
+	  @Override
+	    @SideOnly(Side.CLIENT)
+	    public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced)
+	    {
+	       
+		  
+		tooltip.add("Testing");
+
+	    
+	}
+	@Override
+public ActionResult<ItemStack> onItemRightClick(ItemStack item, World world, EntityPlayer user, EnumHand hand)
+
+{ 
+		RayTraceResult raytrace = user.rayTrace(10, 30);
+		
 	
+	return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, item);
+}
 }
