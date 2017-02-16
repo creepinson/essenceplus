d2b0d1da4b4749e6273024879dfe39e2b187f836
dafuq360 <theoplaysmc@gmail.com>
2017-02-16 12:37:21 -0800

    1

--- src/main/java/me/creepinson/Main/ClientProxy.java
+++ src/main/java/me/creepinson/Main/ClientProxy.java
@@ -5,7 +5,8 @@ import me.creepinson.handlers.ItemHandler;
 public class ClientProxy extends CommonProxy {
 	@Override
 	public void preInit() {
-		  ItemHandler.registerRenders();
+		  super.preInit();
+		ItemHandler.registerRenders();
 			
 	}
 
