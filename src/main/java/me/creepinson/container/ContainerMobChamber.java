a58ddccbaa9f7c61c18715dda081a3f6ebb29ddc
dafuq360 <theoplaysmc@gmail.com>
2017-02-17 09:30:24 -0800

    f

--- src/main/java/me/creepinson/container/ContainerMobChamber.java
+++ src/main/java/me/creepinson/container/ContainerMobChamber.java
@@ -16,15 +16,15 @@ public class ContainerMobChamber extends Container{
 	public ContainerMobChamber(IInventory playersinv, TileEntityMobChamber te){
 	this.te = te;
 		IItemHandler handler = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
-	this.addSlotToContainer(new Slot(playersinv, 0, 62, 17));
-	this.addSlotToContainer(new Slot(playersinv, 1, 80, 17));
-	this.addSlotToContainer(new Slot(playersinv, 2, 98, 17));
-	this.addSlotToContainer(new Slot(playersinv, 3, 62, 35));
-	this.addSlotToContainer(new Slot(playersinv, 4, 80, 35));
-	this.addSlotToContainer(new Slot(playersinv, 5, 98, 35));
-	this.addSlotToContainer(new Slot(playersinv, 6, 62, 53));
-	this.addSlotToContainer(new Slot(playersinv, 7, 80, 53));
-	this.addSlotToContainer(new Slot(playersinv, 8, 98, 53));
+	this.addSlotToContainer(new Slot(te, 0, 62, 17));
+	this.addSlotToContainer(new Slot(te, 1, 80, 17));
+	this.addSlotToContainer(new Slot(te, 2, 98, 17));
+	this.addSlotToContainer(new Slot(te, 3, 62, 35));
+	this.addSlotToContainer(new Slot(te, 4, 80, 35));
+	this.addSlotToContainer(new Slot(te, 5, 98, 35));
+	this.addSlotToContainer(new Slot(te, 6, 62, 53));
+	this.addSlotToContainer(new Slot(te, 7, 80, 53));
+	this.addSlotToContainer(new Slot(te, 8, 98, 53));
 		
 		int xPos = 8;
 		int yPos = 84;
@@ -33,17 +33,17 @@ public class ContainerMobChamber extends Container{
 		
 		for (int y = 0; y < 3; ++y) {
 			for (int x = 0; x < 9; ++x) {
-				this.addSlotToContainer(new Slot(playersinv, x + y * 9 + 9, xPos + x * 18, yPos + y * 18));
+				this.addSlotToContainer(new Slot(te, x + y * 9 + 9, xPos + x * 18, yPos + y * 18));
 			}
 		}
 				
 		for (int x = 0; x < 9; ++x) {
-			this.addSlotToContainer(new Slot(playersinv, x, xPos + x * 18, yPos + 58));
+			this.addSlotToContainer(new Slot(te, x, xPos + x * 18, yPos + 58));
 		}
 }
 	@Override
 	public boolean canInteractWith(EntityPlayer playerIn) {
-	TileEntityMobChamber.isUseableByPlayer(playerIn);
+	te.isUseableByPlayer(playerIn);
 		return true;
 	}
 
